f79a39fd3bea80dbd71f27bdeb5d948638478dea
Irine Tuzova <85580864+tuzova@users.noreply.github.com>
2021-07-16 20:44:14 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int account = 185;
         int credit = 1_999;
+        //short version:
         //int total = credit > 1_000 ? account + credit + (credit/100) : account + credit;
         int total;
         if (credit > 1_000) {
