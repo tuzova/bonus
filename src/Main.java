f289cba891a897fe87cf1636ff629751b75410c5
Irine Tuzova <85580864+tuzova@users.noreply.github.com>
2021-07-16 20:42:51 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,13 @@ public class Main {
     public static void main(String[] args) {
         int account = 185;
         int credit = 1_999;
+        //int total = credit > 1_000 ? account + credit + (credit/100) : account + credit;
         int total;
         if (credit > 1_000) {
             total = account + credit + (credit/100);
         } else {
             total = account + credit;
         }
-        //int total = credit > 1_000 ? account + credit + (credit/100) : account + credit;
         System.out.println(total);
         }
 }
